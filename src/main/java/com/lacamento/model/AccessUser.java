5d7e5756401e87096d373a61452204d244cc1b2c
Jefferson Luiz Cruz <jefferson.luiz.cruz@gmail.com>
2021-01-30 23:49:52 -0300

    Finalização das classe AccesUser e Permission
    AccesUser e Permission

--- src/main/java/com/lacamento/model/AccessUser.java
+++ src/main/java/com/lacamento/model/AccessUser.java
@@ -1,5 +1,43 @@
 package com.lacamento.model;
 
-public class AccessUser {
+import java.io.Serializable;
+import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Getter @Setter
+@Entity(name = "user")
+public class AccessUser implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String email;
+	private String password;
+
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "user_permission", 
+	           joinColumns = @JoinColumn(referencedColumnName = "user_id"),
+	           inverseJoinColumns = @JoinColumn(name = "permission_id"))
+	private List<Permission> permission;
 }
--- src/main/java/com/lacamento/model/Permission.java
+++ src/main/java/com/lacamento/model/Permission.java
@@ -1,5 +1,30 @@
 package com.lacamento.model;
 
-public class Permission {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Getter @Setter
+@Entity(name = "permission")
+public class Permission implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String description;
 
 }
