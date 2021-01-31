7916f966b66ada4bb1b3b4ae4d97eea05390ddba
Jefferson Luiz Cruz <jefferson.luiz.cruz@gmail.com>
2021-01-30 23:16:41 -0300

    Finalizada classe Entry
    Concluído a classe Entry

--- src/main/java/com/lacamento/model/Entry.java
+++ src/main/java/com/lacamento/model/Entry.java
@@ -1,5 +1,67 @@
 package com.lacamento.model;
 
-public class Entry {
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.sun.istack.NotNull;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Getter @Setter
+@Entity(name = "entry")
+public class Entry implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long codigo;
+	
+	@NotNull
+	private String description;
+
+	@NotNull
+	@Column(name = "due_date")
+	private LocalDate dueDate;
+
+	@Column(name = "payment_date")
+	private LocalDate paymentDate;
+
+	@NotNull
+	private BigDecimal amount;
+
+	private String observation;
+
+	@NotNull
+	@Enumerated(EnumType.STRING)
+	private TypeEntry type;
+
+	@NotNull
+	@ManyToOne
+	@JoinColumn(name = "category_id")
+	private Category category;
+
+	@NotNull
+	@ManyToOne
+	@JoinColumn(name = "person_id")
+	private Person person;
 
 }
