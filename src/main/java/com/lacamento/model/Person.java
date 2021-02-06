ac6f9010ad0b25396134266d1217db63483b954e
Jefferson <jefferson.luiz.cruz@gmail.com>
2021-02-06 17:04:20 -0300

    Conexão com Gitkraken

--- src/main/java/com/lacamento/model/Person.java
+++ src/main/java/com/lacamento/model/Person.java
@@ -20,7 +20,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode
-@Getter @Setter
+@Getter @Setter			///Teste de conexão com Gitkraken
 @Entity
 public class Person implements Serializable{
 	private static final long serialVersionUID = 1L;
